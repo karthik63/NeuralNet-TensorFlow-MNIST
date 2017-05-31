5129204cee02e9301ccd74c5def42ea63a0ed217
karthik63 <twoindeed@gmail.com>
2017-05-31 19:28:47 +0530

    finished find_loss_per_batch()

--- conv_net.py
+++ conv_net.py
@@ -5,10 +5,11 @@ import inputs
 
 class NeuralNet:
 
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, w):
         self.X_train_set, self.Y_train_set_n, self.X_valid_set, self.Y_valid_set_n, self.X_test_set, self.Y_test_set_n,\
             self.Y_train_set_v, self.Y_valid_set_v, self.Y_test_set_v = inputs.get_inputs('mnist.pkl.gz')
 
+        self.max_epochs = w
         self.n_layers = x
         self.n_perceptrons_per_layer = y
         self.batch_size = z
@@ -18,6 +19,7 @@ class NeuralNet:
                            'biases':tf.random_normal(self.n_perceptrons_per_layer[1])}
         self.weights[self.n_layers - 1] = {'weights':tf.random_normal(self.n_perceptrons_per_layer[self.n_layers - 2], 10),
                                            'biases':tf.random_normal(10)}
+        self.batch_loss = None
 
         for i in range(2, self.n_layers - 1):
             self.weights[i] = {'weights':tf.random_normal((self.n_perceptrons_per_layer[i - 1]), self.n_perceptrons_per_layer[i])
@@ -35,6 +37,17 @@ class NeuralNet:
 
         return self.layer_activations[self.n_layers - 1]
 
-    def train(self, X, Y):
+    def find_loss_per_batch(self, X, Y):
 
-        for i in ran
+        self.batch_loss = 0
+
+        for i in range(self.batch_size):
+            prediction = self.predict(X[i])
+            self.batch_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(prediction, Y[i]))
+
+        return self.batch_loss
+
+    def train(self):
+
+        for in range(self.max_epochs):
+            
