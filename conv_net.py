8988beeed341f3ce2c7f02063f4e2981783c18c1
karthik63 <twoindeed@gmail.com>
2017-06-01 18:26:53 +0530

    working but not learning

--- conv_net.py
+++ conv_net.py
@@ -12,9 +12,6 @@ class NeuralNet:
 
         self.n_training_examples = self.X_train_set.shape[0]
 
-        self.X_train_set = tf.constant(self.X_train_set, dtype=tf.float32)
-        self.Y_train_set_v = tf.constant(self.Y_train_set_v, dtype=tf.float32)
-
         self.max_epochs = w
         self.n_layers = x
         self.n_perceptrons_per_layer = y
@@ -36,7 +33,7 @@ class NeuralNet:
 
     def predict(self, X):
 
-        self.layer_activations[0] = tf.Variable(X)
+        self.layer_activations[0] = X
 
         for i in range(1, self.n_layers):
             self.layer_activations[i] = tf.nn.relu(tf.add(tf.matmul(self.layer_activations[i - 1], self.weights[i]['weights']),
@@ -44,21 +41,22 @@ class NeuralNet:
 
         return self.layer_activations[self.n_layers - 1]
 
-    def find_loss_per_batch(self):
-
-        X = tf.placeholder(dtype='float32', shape=(None, 784))
-        Y = tf.placeholder(dtype='float32', shape=(None, 10))
-
-        self.batch_loss = 0
+    def find_loss_per_batch(self, X, Y):
 
         prediction = self.predict(X)
-        aa = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction, labels=Y))
+        batch_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction, labels=Y))
 
-        return aa
+        return batch_loss
 
     def train(self):
 
-        optimiser = tf.train.AdamOptimizer(learning_rate=.001).minimize(self.find_loss_per_batch())
+        X = tf.placeholder(dtype='float32', shape=(None, 784), name='X_batch')
+        Y = tf.placeholder(dtype='float32', shape=(None, 10), name='Y_batch')
+
+        batch_loss = self.find_loss_per_batch(X, Y)
+
+        optimiser = tf.train.AdamOptimizer(learning_rate=.001).minimize(batch_loss)
+
         n_updates_per_epoch = int(self.n_training_examples / self.batch_size)
 
         with tf.Session() as sess:
@@ -68,13 +66,23 @@ class NeuralNet:
                 for j in range(n_updates_per_epoch):
 
                     current_batch_X = self.X_train_set[j * self.batch_size : j * self.batch_size + self.batch_size]
-                    current_batch_Y = self.Y_test_set_v[j * self.batch_size : j * self.batch_size + self.batch_size]
+                    current_batch_Y = self.Y_train_set_v[j * self.batch_size : j * self.batch_size + self.batch_size]
 
                     sess.run(tf.global_variables_initializer())
-                    _, el = sess.run([optimiser, epoch_loss], feed_dict={X: current_batch_X, Y: current_batch_Y})
+                    _, temp = sess.run([optimiser, batch_loss], feed_dict={X: current_batch_X, Y: current_batch_Y})
+                    epoch_loss += temp
+                print('epoch loss is ' + str(epoch_loss))
 
-                print('epoch loss is ' + str(el / self.n_training_examples))
+    def find_accuracy(self):
+        with tf.Session() as sess:
+            a = sess.eval(self.predict(self.X_test_set))
+        b = self.Y_test_set_v
+
+        c = a == b
+
+        print((sum(c)/b.shape[0]) * 100)
 
 nn1 = NeuralNet(3, [784, 10, 10], 100, 10)
 
 nn1.train()
+nn1.find_accuracy()
